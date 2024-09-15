68fccec308044bdeb4afd27fbac3e7d34c7cb394
darnedog <danielaarnedog@gmail.com>
2024-09-15 12:52:46 -0500

    fix: setup github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://darnedog.github.io',
+  base: '/',
+  outDir: 'public',
+  publicDir: 'static',
 });
