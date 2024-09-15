9ee9adc6403db7b6f15ae18dad798391bf42301b
darnedog <danielaarnedog@gmail.com>
2024-09-15 12:58:04 -0500

    feat: include /docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://darnedog.github.io',
-  base: '/',
-  outDir: 'public',
-  publicDir: 'static',
+  output: 'static',
+  outDir: './docs',
+  build: {
+    assets: 'astro'
+  }
 });
